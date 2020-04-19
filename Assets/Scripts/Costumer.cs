7816387947d4661e3dbe08ae14d63b7dc3b4c29a
Fran Bosquet <frbosquet@gmail.com>
2020-04-19 20:34:04 +0200

    Costumer could move to places programatically

---
+++ Assets/Scripts/Costumer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Costumer : MonoBehaviour
+{
+  public float moveSpeed = 1;
+
+  private Vector3 targetPosition;
+  private bool shouldMove = true;
+
+  private void Update()
+  {
+    if (shouldMove)
+    {
+      Vector3 direction = (targetPosition - transform.position);
+      direction.Normalize();
+
+      transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+      if (Vector3.Distance(targetPosition, transform.position) < 0.5f)
+      {
+        shouldMove = false;
+      }
+    }
+  }
+
+  public void SetTargetPosition(Vector3 newPosition)
+  {
+    targetPosition = newPosition;
+    shouldMove = true;
+  }
+}
