1fe9699a027839f406ee75f95ce7c5b7a5f42455
Fran Bosquet <frbosquet@gmail.com>
2020-05-25 12:47:06 +0200

    Make customers join the queue

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+  public float moveSpeed = 2;
+  [Range(0f, 1f)]
+  public float joiningThreshold = 0.5f;
+
+  [SerializeField]
+  private Vector3 targetPosition;
+  private bool shouldMove = false;
+  private Queue queue;
+
+  private void Start()
+  {
+    queue = GameObject.Find("queue").GetComponent<Queue>();
+
+    if (ShouldQueue())
+    {
+      queue.Join(this);
+    }
+  }
+
+  private void Update()
+  {
+    if (shouldMove)
+    {
+      Vector3 direction = (targetPosition - transform.position);
+      direction.Normalize();
+
+      transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+      if (Vector3.Distance(targetPosition, transform.position) < 0.1f)
+      {
+        shouldMove = false;
+      }
+    }
+  }
+
+  public void SetTargetPosition(Vector3 newPosition)
+  {
+    targetPosition = newPosition;
+    shouldMove = true;
+  }
+
+  private bool ShouldQueue()
+  {
+    if (Random.Range(0f, 1f) > joiningThreshold)
+    {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ Assets/Scripts/Queue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queue : MonoBehaviour
+{
+  public Vector3 offset;
+  public List<Customer> customers;
+
+  private void UpdatePositions()
+  {
+    for (int i = 0; i < customers.Count; i++)
+    {
+      Customer customer = customers[i];
+      customer.SetTargetPosition(transform.position + offset * i);
+    }
+  }
+
+  public void Join(Customer customer)
+  {
+    for (int i = 0; i <= customers.Count; i++)
+    {
+      if (i == customers.Count)
+      {
+        customers.Add(customer);
+        break;
+      }
+
+      Customer other = customers[i];
+
+      float otherDist = (other.transform.position - transform.position).magnitude;
+      float distance = (customer.transform.position - transform.position).magnitude;
+
+      if (distance < otherDist)
+      {
+        customers.Insert(i, customer);
+        break;
+      }
+    }
+
+    UpdatePositions();
+  }
+}
